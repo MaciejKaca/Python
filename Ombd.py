26726227927c27ce56180552388b6c8fea9e000f
MaciejKaca <maciek.kaca@gmail.com>
2018-08-21 17:16:49 +0200

    DO NOT LOOK. WORK IN PROGRESS

--- Ombd.py
+++ Ombd.py
@@ -1,12 +1,13 @@
 import omdb
 import json
+import operator
 
 def search_movie(title):
     omdb.set_default('apikey', '463a3dd')
-    res = omdb.request(t=title)
-    xml_content = res.content
-    movie = json.loads(xml_content)
-    print(json.dumps(movie, indent=4))
+    res = omdb.title(title)
+    #xml_content = res.content
+    #movie = json.loads(xml_content)
+    return (res)
 
 def _input():
     movies = []
@@ -14,16 +15,41 @@ def _input():
     while user_input != 'end':
         print(','.join(movies))
         user_input = input("Enter movie: ")
-        if user_input != 'end':
-            for movie in user_input.split(',', len(user_input.split())):
-                movie = movie.strip()
-                if movie in movies:
-                    print("Some movies are repeated")
-                else:
-                    movies.append(movie)
+
+        type_sort=(user_input.split(':'))[-1]
+        type_sort=type_sort.strip()
+
+        if type_sort!="Rating" and type_sort!="Lenght" and type_sort!="Release date" and type_sort!="Popularity":
+            if user_input != 'end':
+                movies = (user_input.split(':'))[0]
+                movies = movies.split(',')
+                for movie in movies:
+                    movie = movie.strip()
+                    if movie in movies:
+                        print("Some movies are repeated")
+                    else:
+                        movies.append(movie)
+        else:
+            if user_input != 'end':
+                movies = (user_input.split(':'))[0]
+                movies = movies.split(',')
+                for movie in movies:
+                    movie = movie.strip()
+                    if movie in movies:
+                        print("Some movies are repeated")
+                    else:
+                        movies.append(movie)
+                        movies.append(type_sort)
     return movies
 
-
+movies_sort = {}
 movies = _input()
+
 for movie in movies:
-    search_movie(movie)
+    if movies[-1] == 'Rating':
+        movies_sort[movie] = (search_movie(movie))['imdb_rating']
+
+sorted_x = sorted(movies_sort.items(), key=operator.itemgetter(1))
+
+for x in sorted_x:
+    print(search_movie(x[0]))
---
+++ Valid_braces.py
@@ -0,0 +1,12 @@
+def valid_braces(sentence):
+    '''
+    Function takes a string of braces, and determines if the order of the braces is valid.
+    It should return true if the string is valid, and false if it's invalid.
+    A string of braces is considered valid if all braces are matched with the correct brace.
+    '''
+    return sentence
+
+# assert valid_braces("(){}[]") == True, "Wrong answer!"
+# assert valid_braces("([{}])") == True, "Wrong answer!"
+# assert valid_braces("(}") == False, "Wrong answer!"
+# assert valid_braces("[(])") == False, "Wrong answer!"
