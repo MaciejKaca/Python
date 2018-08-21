d4eac0504b20554dcda008326acc50ccf6195eee
MaciejKaca <maciek.kaca@gmail.com>
2018-08-21 11:21:05 +0200

    Shorter version

--- PIN_Regex.py
+++ PIN_Regex.py
@@ -1,10 +1,7 @@
 import re
 
 def validatePIN(PIN):
-    if re.fullmatch(r'[0-9]{4}', PIN):
-        return True
-    else:
-        return False
+    return bool(re.fullmatch(r'[0-9]{4}', PIN))
 
 assert validatePIN("1234") == True, "Wrong validation!"
 assert validatePIN("12345") == False, "Wrong validation!"
