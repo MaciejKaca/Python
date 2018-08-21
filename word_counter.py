e598317e02e3595648f60a244b4c7f50ae68c17e
MaciejKaca <maciek.kaca@gmail.com>
2018-08-21 10:12:16 +0200

    Second version with counting not alphanumeric chars

--- word_counter.py
+++ word_counter.py
@@ -8,5 +8,24 @@ def word_counter(sentence):
             dict[word]=1
     return dict
 
-answer = {"Ala": 2, "ma": 2, "kota.": 1, "psa.": 1}
-assert word_counter("Ala ma kota. Ala ma psa.") == answer
+def word_counter2(sentence):
+    new_sentence = "".join(c for c in sentence if (c.isalnum() or c.isspace()) )
+    another_chars = [c for c in sentence if not(c.isalnum() or c.isspace()) ]
+    dict = {}
+    for word in new_sentence.split():
+        if word in dict:
+            dict[word]+=1
+        else:
+            dict[word]=1
+
+    for word in another_chars:
+        if word in dict:
+            dict[word]+=1
+        else:
+            dict[word]=1
+    return dict
+
+print(word_counter2("Ala ma kota. )))))))))Ala ma psa."))
+
+#answer = {"Ala": 2, "ma": 2, "kota.": 1, "psa.": 1}
+#assert word_counter("Ala ma kota. Ala ma psa.") == answer
